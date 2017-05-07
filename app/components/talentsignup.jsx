d1796b46766dd9205385e0eb7301a21a8961d867
davisk4rpi <davisk4rpi@gmail.com>
2017-05-06 23:31:01 -0700

    add detail to talent signup sheet

--- app/components/talentsignup.jsx
+++ app/components/talentsignup.jsx
@@ -1,25 +1,36 @@
 var React = require('react');
-import { Row, Col } from 'react-materialize';
+import { Row, Col, Input } from 'react-materialize';
 
 var talentSignup= React.createClass({
   render: function(){
     return (
-      <div className="container center-align">
+      <div class="container center-align">
         <Row>
           <Col s={12}>
-            <h1>Welcome to EFlash!</h1>
-            <h3>Tagline goes here</h3>
+            <h2>Talent Sign Up Form</h2>
           </Col>
-          <Row>
-            <Col s={12}>
-              <a className="waves-effect waves-light btn amber darken-1 white-text" href="talent/signup.html">Sign Up to Get Hired</a>
-            </Col>
-          </Row>
-          <Row>
-            <Col s={12}>
-              <a className="waves-effect waves-light btn amber darken-1 white-text" href="recruiter/signup.html">Sign Up to Find the Best Talent</a>
-            </Col>
-          </Row>
+        </Row>
+        <Row>
+          <Col s={12}>
+              <div class="row valign-wrapper flow-text">
+                <div class="col s4">
+                  <img src="../img/IMDb-logo.png" class="imdb_signup_logo"/>
+                </div>
+                <div class="col s8">
+                  <p>
+                    Enter a link to your IMDb Page and we'll make sure that all of your credits are up to date
+                  </p>
+                </div>
+              </div>
+              <Input s={12} id="imdb_url" class="validate" type="text" label="IMDb URL" />
+              <Input s={12} id="email" class="validate" type="email" label="Email" />
+              <Input s={12} id="password" class="validate" type="password" label="password" />
+              <Input s={12} id="confirm-password" class="validate" type="password" label="password" />
+          </Col>
+          <Col s={12}>
+            <a class="waves-effect waves-light btn" href="#">Cancel</a>
+            <a class="waves-effect waves-light btn" href="profile.html">Sign Up!</a>
+          </Col>
         </Row>
       </div>
     )
