f7771960976d0ba7abb8a032e38f97eb371dd6a2
Herminio Garcia <hermano360@gmail.com>
2017-05-07 05:46:59 -0700

    minor fix on past queries for clarity

--- app/components/Querypast.jsx
+++ app/components/Querypast.jsx
@@ -35,6 +35,7 @@ var Querypast= React.createClass({
       <div>
         <Col m={6} s={12}>
           <Card className='blue-grey darken-1' textClassName='white-text' title='Remember The Titans - Lead' onClick={this.onSearch1}>
+            <p>Requirements:</p>
             <p>Actor Credits:10</p>
             <p> Writer Credits: 6</p>
             <p>Union: N, Travel: N, Passport: N</p>
@@ -42,6 +43,7 @@ var Querypast= React.createClass({
         </Col>
         <Col m={6} s={12}>
           <Card className='blue-grey darken-1' textClassName='white-text' title='Sleeping Beauty - Stunt Double' onClick={this.onSearch2}>
+            <p>Requirements:</p>
             <p>Actor Credits:2</p>
             <p> Writer Credits: 1</p>
             <p>Union: Y, Travel: N, Passport: Y</p>
@@ -49,6 +51,7 @@ var Querypast= React.createClass({
         </Col>
         <Col m={6} s={12}>
           <Card className='blue-grey darken-1' textClassName='white-text' title='Tarzan - Director' onClick={this.onSearch3}>
+            <p>Requirements:</p>
             <p>Actor Credits:14</p>
             <p> Writer Credits: 30</p>
             <p>Union: Y, Travel: Y, Passport: Y</p>
